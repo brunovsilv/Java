b7f2a458dbcf652b42ccda1e5afef26f5ebc6e8f
Bruno Vinicius da Silva Corsini <169068441+brunovsilv@users.noreply.github.com>
2024-09-02 12:16:22 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+  public static void main(String[] args) {
+int numerador = 16;
+  int denominador = 4;
+    int resto = numerador % denominador;
+
+    System.out.println("o resto é: " + resto);
+  }
+}
